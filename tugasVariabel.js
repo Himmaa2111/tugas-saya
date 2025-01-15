744afb3dd50fe60f528e803a3fa7765053ee4dad
Himmaa2111 <anitazakiyya@gmail.com>
2025-01-15 10:57:54 +0700

    Add files via upload

---
+++ tugasVariabel.js
@@ -0,0 +1,16 @@
+let nama = "Himmatul Ulya";
+let alamat = "Dempet,Tugu";
+let usia = "16";
+let ttl = "Demak, 21 November 2008";
+let gender = "perempuan";
+let agama = "islam";
+let email = "anitazakiyya@gmail.com";
+let berat = "35kg";
+let tinggi = "148cm";
+let hobi = "rebahan";
+let cita = "Bidan";
+let pekerjaan = "pelajar";
+let sekolah = "SMKN 1 Sayung";
+let jurusan = "PPLG";
+
+document.write("nama saya adalah "+ nama+ ", saya tinggal di "+ alamat+ " , pada tahun 2024 ini usia saya adalah "+ usia+" tahun, saya lahir di "+ ttl+", jeis kelamin saya adalah "+ gender+", saya beragama "+ agama+" , Alamat email saya adalah "+ email+" , saya memiliki berat "+ berat+" , dengan tinggi "+ tinggi+" , saya sangat senang "+ hobi+", karena saya memiliki cita cita sebagai "+ cita+" , saat ini saya adalah seorang "+ pekerjaan+" yang bersekolah di "+ sekolah+" pada jurusan "+ jurusan+"" )
